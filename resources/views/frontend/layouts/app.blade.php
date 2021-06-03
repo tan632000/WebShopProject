21e5ca3deadd303aec4fcb4988093b6d1d8b7771
Tan Hoang <tan.hoang@skyblazestudios.com>
2021-06-03 09:06:04 +0700

    add js

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -249,6 +249,7 @@
         </div>
     </div>
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/index.js') }}" defer></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </body>
 </html>
