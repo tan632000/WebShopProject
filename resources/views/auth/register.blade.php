03a55e3dc0e552bce576334b8eee870c3739c265
Tan Hoang <tan.hoang@skyblazestudios.com>
2021-06-17 10:55:42 +0700

    add category

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,80 @@
-@extends('layouts.app')
 
+@extends('layouts.admin_layout')
+@section('title')
+    Register
+@endsection
 @section('content')
+@endsection
+
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
+    <div class="card o-hidden border-0 shadow-lg my-5">
+        <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+            {{--<div class="col-lg-5 d-none d-lg-block bg-register-image"></div> --}}
+                <div class="col-lg-8 mx-auto">
+                    <div class="p-5">
+                        <div class="text-center">
+                            <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+                        </div>
+                        <form method="POST" action="{{ route('register') }}">
                         @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
+                            <div class="form-group ">
+                                <input type="text" class="form-control form-control-user @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name') }}"
+                                    required autocomplete="name" autofocus placeholder="Enter  Your Name">
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
+                            <div class="form-group">
+                                <input type="email" id="email" class="form-control form-control-user @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"
+                                    required autocomplete="email" placeholder="Email Address">
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
+                            <div class="form-group row">
+                            <div class="col-sm-6 mb-3 mb-sm-0">
+                                <input type="password" class="form-control form-control-user @error('password') is-invalid @enderror" id="password" name="password"
+                                    required autocomplete="new-password" placeholder="Password">
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            <div class="col-sm-6">
+                                <input type="password" class="form-control form-control-user" name="password_confirmation" required autocomplete="new-password"
+                                     placeholder="Repeat Password">
+                                </div>
                             </div>
+                            <button class="btn btn-primary btn-user btn-block">
+                                {{ __('Register') }}
+                            </button>
+                            <hr>
+                            <a href="index.html" class="btn btn-google btn-user btn-block">
+                                <i class="fab fa-google fa-fw"></i> Register with Google
+                            </a>
+                            <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                                <i class="fab fa-facebook-f fa-fw"></i> Register with Facebook
+                            </a>
+                        </form>
+                        <hr>
+                        <div class="text-center">
+                            <a class="small" href="{{route('password.request')}}">Forgot Password?</a>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
+                        <div class="text-center">
+                            <a class="small" href="{{route('login')}}">Already have an account? Login!</a>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-@endsection
+
+
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/trangchu', function () {
     return view('frontend.home');
@@ -50,3 +50,70 @@ Route::get('/lienhe', function () {
     return view('frontend.lienhe');
 });
 
+//-----------------------------ADMIN ROUTE------------------------------------
+//  CATEGORY ROUTE
+Route::get('/add/category', [App\Http\Controllers\CategoryController::class, 'show_category_form'])->name('add_category_form');
+Route::post('/add/category', [App\Http\Controllers\CategoryController::class, 'add_category'])->name('category_add');
+Route::get('/category/manage', [App\Http\Controllers\CategoryController::class, 'manage_category'])->name('manage_category');
+Route::get('/category/unpublish/{id}', [App\Http\Controllers\CategoryController::class, 'category_unpublish'])->name('category_unpublish');
+Route::get('/category/publish/{id}', [App\Http\Controllers\CategoryController::class,'category_publish'])->name('category_publish');
+Route::get('/category/category_delete/{id}', [App\Http\Controllers\CategoryController::class,'category_delete'])->name('category_delete');
+Route::get('/category/category_edit/{id}', [App\Http\Controllers\CategoryController::class,'category_edit'])->name('category_edit');
+Route::post('/category/category_update', [App\Http\Controllers\CategoryController::class,'category_update'])->name('category_update');
+
+//  PRODUCT ROUTE
+Route::get('/add/product', 'ProductController@show_product_form')->name('add_product_form');
+Route::post('/add/product', 'ProductController@add_product')->name('product_add');
+Route::get('/product/manage',[
+    'uses' => 'ProductController@manage_product',
+    'as' => 'manage_product'
+]);
+Route::get('/product/unpublish/{id}',[
+    'uses' => 'ProductController@product_unpublish',
+    'as' => 'unpublished_product'
+]);
+Route::get('/product/publish/{id}',[
+    'uses' => 'ProductController@product_publish',
+    'as' => 'published_product'
+]);
+Route::get('/product/product_edit/{id}',[
+    'uses' => 'ProductController@product_edit',
+    'as' => 'product_edit'
+]);
+Route::post('/product/product_update',[
+    'uses' => 'ProductController@product_update',
+    'as' => 'product_update'
+]);
+Route::get('/product/product_restore/{id}',[
+    'uses' => 'ProductController@product_restore',
+    'as' => 'product_restore'
+]);
+Route::get('/product/product_forceDelete/{id}',[
+    'uses' => 'ProductController@product_forceDelete',
+    'as' => 'product_forceDelete'
+]);
+
+
+//  ORDER ROUTE
+Route::get('/product/details/{id}',[
+    'uses' => 'FrontendController@product_details_by_id',
+    'as'   =>'product_details'
+]);
+Route::get('/manage/order',[
+    'uses' => 'OrderController@manage_order_info',
+    'as'   =>'manage_order'
+]);
+Route::get('/order/details/{id}',[
+    'uses' => 'OrderController@order_detail_by_order_id',
+    'as'   =>'order_details'
+]);
+Route::get('/order/invoice/{id}',[
+    'uses' => 'OrderController@order_invoice_view_by_order_id',
+    'as'   =>'order_invoice'
+]);
+
+// Phiếu thanh toán được in
+Route::get('/order/invoice/download/{id}',[
+    'uses' => 'OrderController@order_invoice_download_by_order_id',
+    'as'   =>'order_invoice_download'
+]);
